2891b57c390f4c4f0afffc6cde2ed17a13cf2101
39LPeter <wlone3949@gmail.com>
2024-06-03 07:25:31 -0700

    API

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
-"""
-    Uses the fake API to get an employer and export the info in json formater
-"""
+"""Exports to-do list information of all employees to JSON format."""
 import json
 import requests
-from sys import argv
 
 if __name__ == "__main__":
-    id_em = argv[1]
-    url_employ = "https://jsonplaceholder.typicode.com/users/{}".format(id_em)
-    url_todos = url_employ + "/todos"
-    r_employ = requests.get(url_employ).json()
-    r_todos = requests.get(url_todos).json()
-    username = r_employ.get("username")
-    total_num_task = r_todos
-    list_dict_report = []
-    for task in total_num_task:
-        id_report = {}
-        id_report["username"] = str(username)
-        id_report["completed"] = task.get("completed")
-        id_report["task"] = str(task.get("title"))
-        list_dict_report.append(id_report)
-    report = {}
-    report[id_em] = list_dict_report
-    with open("{}.json".format(id_em), "w") as fjson:
-        fjson.write(json.dumps(report))
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
+
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
